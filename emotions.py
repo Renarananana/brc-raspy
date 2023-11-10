ae1ea44ad69825160a5928fd5660bec4949395fe
Renato Andaur <42723640+Renaranananna@users.noreply.github.com>
2023-11-10 16:32:40 +0000

    Update emotions.py

--- emotions.py
+++ emotions.py
@@ -77,6 +77,7 @@ if mode == "display":
     # start the webcam feed
     cap = cv2.VideoCapture(0)
     t0 = time.time()
+    c = 0
     while True:
         # Find haar cascade to draw bounding box around face
         ret, frame = cap.read()
@@ -84,22 +85,26 @@ if mode == "display":
         t0 = time.time()
         delta_time = t0 - prev_t
         fps = 1/delta_time
-
+        
         fps = str(int(fps))
         cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
+        
         if not ret:
             break
-        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+        if c%2 == 0:
+            facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
 
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
             roi_gray = gray[y:y + h, x:x + w]
             cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-            prediction = model.predict(cropped_img)
-            maxindex = int(np.argmax(prediction))
-            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            if c%2 == 0:
+                prediction = model.predict(cropped_img, verbose = 0)
+                maxindex = int(np.argmax(prediction))
+            if prediction is not None:
+                cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
         if cv2.waitKey(1) & 0xFF == ord('q'):
