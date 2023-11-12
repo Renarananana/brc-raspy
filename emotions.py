ee92ce40223ea4873030af251e433b21fae9f71b
Renaranananna <Renato.andaur@gmail.com>
2023-11-12 17:21:31 -0300

    feat: Add diffrent thread for higher fps

--- emotions.py
+++ emotions.py
@@ -9,106 +9,97 @@ from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import time
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import threading
 
-# command line argument
-ap = argparse.ArgumentParser()
-ap.add_argument("--mode",help="train/display")
-a = ap.parse_args()
-mode = a.mode 
+# prevents openCL usage and unnecessary logging messages
+cv2.ocl.setUseOpenCL(False)
+# dictionary which assigns each label an emotion (alphabetical order)
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
 
-# Define data generators
-#train_dir = 'data/train'
-#val_dir = 'data/test'
+class EmotionDetector():
+    def __init__(self) -> None:
+        self.frame = None
+        self.video = True
+        self.faces = []
+        self.prediction = None
 
-num_train = 28709
-num_val = 7178
-batch_size = 64
-num_epoch = 50
+        self.model = Sequential()
 
-#train_datagen = ImageDataGenerator(rescale=1./255)
-#val_datagen = ImageDataGenerator(rescale=1./255)
+        self.model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+        self.model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
 
-#train_generator = train_datagen.flow_from_directory(
-#        train_dir,
-#        target_size=(48,48),
-#        batch_size=batch_size,
-#        color_mode="grayscale",
-#        class_mode='categorical')
+        self.model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
 
-#validation_generator = val_datagen.flow_from_directory(
-#        val_dir,
-#        target_size=(48,48),
-#        batch_size=batch_size,
-#        color_mode="grayscale",
-#        class_mode='categorical')
+        self.model.add(Flatten())
+        self.model.add(Dense(1024, activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(7, activation='softmax'))
 
-# Create the model
-model = Sequential()
+        # If you want to train the same self.model or try other models, go for this
+        # emotions will be displayed on your face from the webcam feed
+        self.model.load_weights('model.h5')
 
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(7, activation='softmax'))
-
-# If you want to train the same model or try other models, go for this
-# emotions will be displayed on your face from the webcam feed
-if mode == "display":
-    model.load_weights('model.h5')
-
-    # prevents openCL usage and unnecessary logging messages
-    cv2.ocl.setUseOpenCL(False)
-
-    # dictionary which assigns each label an emotion (alphabetical order)
-    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
-
-    # start the webcam feed
-    cap = cv2.VideoCapture(0)
-    t0 = time.time()
-    c = 0
-    while True:
-        # Find haar cascade to draw bounding box around face
-        ret, frame = cap.read()
-        prev_t = t0
+        
+    
+    def start_video(self):
+        # start the webcam feed
+        self.cap = cv2.VideoCapture(0)
         t0 = time.time()
-        delta_time = t0 - prev_t
-        fps = 1/delta_time
-        
-        fps = str(int(fps))
-        cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
-        
-        if not ret:
-            break
-        if c%2 == 0:
-            facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+        self.facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        self.hola = False
+        self.t = threading.Thread(target=self.emotion_detection, args=( ))
+        self.t.start()
+        while True:
+            # Find haar cascade to draw bounding box around face
+            ret, self.frame = self.cap.read()
+            prev_t = t0
+            t0 = time.time()
+            delta_time = t0 - prev_t
+            fps = 1/delta_time
+            
+            fps = str(int(fps))
+            cv2.putText(self.frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
+            
+            if not ret:
+                break
+            for (x, y, w, h) in self.faces:
+                cv2.rectangle(self.frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+                if self.prediction is not None:
+                    maxindex = int(np.argmax(self.prediction))
+                    print(self.prediction)
+                    cv2.putText(self.frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-            if c%2 == 0:
-                prediction = model.predict(cropped_img, verbose = 0)
-                maxindex = int(np.argmax(prediction))
-            if prediction is not None:
-                cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-        cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            cv2.imshow('Video', self.frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-    cap.release()
-    cv2.destroyAllWindows()
+        self.video = False
+        self.t.join()
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+
+
+
+    def emotion_detection(self):
+        while self.frame is None:
+            time.sleep(0.1)
+        while self.video:
+            gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+            self.faces = self.facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+            for (x, y, w, h) in self.faces:
+                roi_gray = gray[y:y + h, x:x + w]
+                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+                self.prediction = self.model.predict(cropped_img, verbose = 0)
+
+ed = EmotionDetector()
+ed.start_video()
+                
